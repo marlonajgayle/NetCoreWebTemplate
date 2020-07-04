656901a3ce2a1a23096694dcc40f788ba0e98a85
Marlon Gayle <marlon.gayle@live.com>
2020-07-04 14:57:05 -0500

    Created SecurityHttpHeaders middleware to inject security headers into response.

---
+++ NetCoreWebTemplate.Api/Filters/SecurityHttpHeadersExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace NetCoreWebTemplate.Api.Filters
+{
+    public static class SecurityHttpHeadersExtensions
+    {
+        public static IApplicationBuilder UseHttpSecurityHeaders(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SecurityHttpHeaders>();
+        }
+    }
+}
